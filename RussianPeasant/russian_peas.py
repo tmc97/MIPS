d86dfe24068ef39a524d21e6758aaa249a4faedf
Taylor Cook <31320478+tmc97@users.noreply.github.com>
2018-02-26 11:36:05 -0600

    Update russian_peas.py
    Added comments to python program and clarified the version of python needed

--- RussianPeasant/russian_peas.py
+++ RussianPeasant/russian_peas.py
@@ -1,21 +1,21 @@
 # Takes two inputted numbers and calculates their product by using the
 # Russian Peasant Multiplication method.
+# Use python3 or else the program hangs
 
 def russian_mult(first_column, second_column):
     result = 0
-
     while (second_column != 1):
         if (second_column % 2 != 0):
             result = first_column + result
-        first_column = (first_column*2)
-        second_column = (second_column//2)
+        first_column = (first_column*2) # Double the first column
+        second_column = (second_column//2) # Divide the second column
 
     result = first_column + result
     return result
 
 def main():
-    first = int(input("Please enter an integer: "))
+    first = int(input("Please enter an integer: ")) # Gets user input
     second = int(input("Please enter a second integer: "))
-    print(russian_mult(first, second))
+    print(russian_mult(first, second)) # Outputs product of the two integers
 
 main()
